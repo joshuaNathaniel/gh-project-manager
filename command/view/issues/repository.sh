f543e6678ebb9317d623bc825ccdbd63d39fc039
Joshua Nathaniel Miller <jmiller@rise8.us>
2021-11-29 07:35:33 -0600

    Added status flag for filtering output

--- command/view/issues/repository.sh
+++ command/view/issues/repository.sh
@@ -4,6 +4,13 @@ set -e
 REPO=$(gh repo view --json name --jq .name)
 OWNER=$(gh repo view --json name,owner --jq .owner.login)
 PROJECT_NUM=$1
+STATUS_TYPES=$3
+
+if [ -z "$STATUS_TYPES" ]; then
+  SELECT_BY_STATUS=
+else
+  SELECT_BY_STATUS="| select(.status==($STATUS_TYPES))"
+fi
 
 QUERY="
   query(\$repo: String!, \$org: String!, \$projectNum: Int!, \$endCursor: String) {
@@ -33,4 +40,4 @@ QUERY="
     }
   }"
 
-exec gh api graphql -f query="${QUERY}" --paginate -F repo="$REPO" -F org="$OWNER" -F projectNum="$PROJECT_NUM" -q "[.data.repository.project.columns.nodes[] as \$columns | \$columns.cards.nodes[] | select(.content != null) | {id: .content.id, title: .content.title, status: \$columns.name}]"
+exec gh api graphql -f query="${QUERY}" --paginate -F repo="$REPO" -F org="$OWNER" -F projectNum="$PROJECT_NUM" -q "[.data.repository.project.columns.nodes[] as \$columns | \$columns.cards.nodes[] | select(.content != null) | {id: .content.id, title: .content.title, status: \$columns.name} $SELECT_BY_STATUS]"
